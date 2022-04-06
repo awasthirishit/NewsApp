86efdeb1dcfa5054592710dffc24e049f5fe095e
Rishit Awasthi <61817675+awasthirishit@users.noreply.github.com>
2022-04-06 12:32:06 +0530

    Update Constants.kt

--- app/src/main/java/com/example/apninews/util/Constants.kt
+++ app/src/main/java/com/example/apninews/util/Constants.kt
@@ -3,6 +3,6 @@ package com.example.apninews.util
 class Constants {
     companion object {
         const val BASE_URL = "https://newsapi.org"
-        const val API_KEY  = "0e4c6aef1d1b409296ba918c63693597"
+        const val API_KEY  = "*******************"
     }
 }
